64b9439accb6640b46e5fc39fcd6f102274c5954
Seiei Miyagi <hanachin@gmail.com>
2015-08-20 14:27:11 +0900

    Add test for to_md

--- test/to_md_test.rb
+++ test/to_md_test.rb
@@ -1,11 +1,21 @@
 require 'test_helper'
 
 class ToMdTest < Minitest::Test
+  using ToMd
+
   def test_that_it_has_a_version_number
     refute_nil ::ToMd::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_array_to_md
+    assert_equal(['one', 'two'].to_md, "- one\n- two")
+  end
+
+  def test_nested_array_to_md
+    assert_equal([['#', 'japanese'], [1, '一']].to_md, <<TABLE)
+| # | japanese |
+| --- | --- |
+| 1 | 一 |
+TABLE
   end
 end
