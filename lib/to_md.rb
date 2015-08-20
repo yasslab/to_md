13f6764ce89a075c9d142f8c21ac1d5c554cc513
Seiei Miyagi <hanachin@gmail.com>
2015-08-20 14:43:39 +0900

    Support nested Array table

--- lib/to_md.rb
+++ lib/to_md.rb
@@ -3,7 +3,17 @@ require "to_md/version"
 module ToMd
   refine(Array) do
     def to_md
-      map {|s| "- #{s}"}.join($/)
+      # list
+      unless all? {|e| e.is_a?(Array) }
+        return map {|s| "- #{s}"}.join($/)
+      end
+
+      # table
+      [
+        "| #{first.join(' | ')} |",
+        "|#{' --- |' * first.size}",
+        drop(1).map {|s| "| #{s.join(' | ')} |"}.join($/)
+      ].join($/) + $/
     end
   end
 end
